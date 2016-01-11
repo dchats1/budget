41e3616aa3a84e805a360ac441903943d5859d99
David Chatterton <david@davidchatterton.com>
2016-01-10 20:17:00 -0500

    Added functions to edit DB

--- budget.py
+++ budget.py
@@ -9,7 +9,7 @@ c = db.cursor()
 
 shouldExit = False
 
-#Create Database
+# Create Database
 def createTable():
 	c.execute('CREATE TABLE income(name TEXT, amount FLOAT)')
 	c.execute('CREATE TABLE constantMonthly(name TEXT, amount FLOAT, month TEXT)')
@@ -17,39 +17,66 @@ def createTable():
 	c.execute('CREATE TABLE varyingMonthly(name TEXT, amount FLOAT, month TEXT)')
 	db.commit()
 
-createTable()
+### Functions to edit database ###
 
-def addIncome():
+def addIncome(): # Add income values
 	name = input('Source of income: ')
-	name = str(name)
+	name = str.lower(name)
 	income = input('Monthly Income: ')
 	income = float(income)
 	c.execute('INSERT INTO income VALUES (?, ?)', (name, income))
-	db.commit()
 
-def editIncome():
+def editIncome(): # Edit income
 	name = input('Which income are you updating?: ')
-	name = str(name)
+	name = str.lower(name)
 	income = input('New value: ')
 	income = float(income)
 	c.execute('UPDATE income SET amount=(?) WHERE name=(?)', (income, name))
-	db.commit()
 
-def addExpenseM():
+def addExpenseM(): # Add Monthly Expense
 	name = input('Monthly Expense name: ')
-	name = str(name)
+	name = str.lower(name)
 	amount = input('Monthly Expense amount: ')
-	amount = float(income)
-	c.execute('INSERT INTO constantMonthly VALUES (?, ?)', (name, amount))
-	db.commit()
+	amount = float(income)	
+	month = input('Month: ')
+	month = str.lower(month)
+	c.execute('INSERT INTO constantMonthly VALUES (?, ?, ?)', (name, amount, month))
 
-def addExpenseY():
+def editExpenseM(): # Edit Monthly Expense
+	name = input(' Expense name: ')
+	name = str.lower(name)
+	amount = input('Monthly Expense amount: ')
+	amount = float(income)	
+	c.execute('UPDATE constantMonthly SET amount=(?) WHERE name=(?)' (amount, name))
+
+
+def addExpenseY(): # Add Yearly Expense
 	name = input('Yearly Expense name: ')
-	name = str(name)
+	name = str.lower(name)
 	amount = input('Yearly Expense amount: ')
 	amount = float(income)
-	c.execute('INSERT INTO constantYearly VALUES (?, ?)', (name, amount))
-	db.commit()
+	month = input('Month: ')
+	month = str.lower(month)
+	c.execute('INSERT INTO constantYearly VALUES (?, ?, ?)', (name, amount, month))
+
+def editExpenseYM(): # Edit Yearly Expense
+	name = input(' Expense name: ')
+	name = str.lower(name)
+	amount = input('Monthly Expense amount: ')
+	amount = float(income)	
+	c.execute('UPDATE constantMonthly SET amount=(?) WHERE name=(?)' (amount, name))
+
+
+def addExpenseV(): # Add montly purchase
+	name = input('Purchase name: ')
+	name = str.lower(name)
+	amount = input('Purchase amount: ')
+	amount = float(income)
+	month = input('Month: ')
+	month = str.lower(month)
+	c.execute('INSERT INTO varyingMonthly VALUES (?, ?, ?)', (name, amount, month))
+
+###
 
 def listIncome():
 	for i in c.execute('select * from income'):
